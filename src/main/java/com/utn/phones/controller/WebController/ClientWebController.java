caf3ea734ce2d24ee43d44b70b1c2378fc25bbf4
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2022-06-14 01:40:19 -0300

    Auth

--- src/main/java/com/utn/phones/controller/WebController/ClientWebController.java
+++ src/main/java/com/utn/phones/controller/WebController/ClientWebController.java
@@ -3,6 +3,7 @@ package com.utn.phones.controller.WebController;
 
 import com.utn.phones.domain.Bill;
 import com.utn.phones.domain.Call;
+import com.utn.phones.domain.User;
 import com.utn.phones.dto.BillDto;
 import com.utn.phones.dto.ClientDto;
 import com.utn.phones.service.CityService;
@@ -10,7 +11,9 @@ import com.utn.phones.service.ClientService;
 import org.hibernate.type.LocalDateType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,11 +38,19 @@ public class ClientWebController {
 
 
     @GetMapping(URL_WEB+"/bill"+"/{idClient}")
-    public ResponseEntity<List<BillDto>> findAllBillByClientRank(@PathVariable("idClient") Integer idClient,
-                                                          @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate from,
-                                                          @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate to){
-        List<BillDto>bills= this.clientService.getBillsByRank(idClient,from,to);
-        return bills.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(bills);
+    public ResponseEntity<List<BillDto>> findAllBillByClientRank(Authentication auth, @PathVariable("idClient") Integer idClient,
+                                                                 @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate from,
+                                                                 @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate to){
+        String username=clientService.findByCode(idClient).getUser().getUsername();
+        User u = (User) auth.getPrincipal();
+        if(u.getUsername().equals(username)){
+            List<BillDto>bills= this.clientService.getBillsByRank(idClient,from,to);
+            return bills.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(bills);
+        }else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+
     }
 
 
